6f1824d4d23c6f8d96dcc5d52292f5ff8da3e1fe
colizej <colizej@gmail.com>
2026-03-28 14:53:25 +0100

    fix(redirects): add cyrillic URL redirects and fix politika

--- config/urls.py
+++ config/urls.py
@@ -22,8 +22,31 @@ urlpatterns = [
     path('about_us', RedirectView.as_view(url='/contacts/', permanent=True)),
     path('delivery/', RedirectView.as_view(url='/contacts/', permanent=True)),
     path('delivery', RedirectView.as_view(url='/contacts/', permanent=True)),
-    path('politika-konfidencialnosti/', RedirectView.as_view(url='/privacy/', permanent=True)),
-    path('politika-konfidencialnosti', RedirectView.as_view(url='/privacy/', permanent=True)),
+    path('politika-konfidencialnosti/', RedirectView.as_view(url='/politika-konfidencialnosti/', permanent=True)),
+    path('politika-konfidencialnosti', RedirectView.as_view(url='/politika-konfidencialnosti/', permanent=True)),
+    
+    # Кириллические URL -> латиница
+    path('%D0%BA%D0%B0%D0%BA-%D0%B0%D0%B4%D0%B0%D0%BF%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F-%D0%BA-%D0%B6%D0%B8%D0%B7%D0%BD%D0%B8-%D0%B2%D0%BE-%D0%92%D1%8C%D0%B5%D1%82%D0%BD%D0%B0%D0%BC%D0%B5/', RedirectView.as_view(url='/kak-adaptirovatsya-k-zhizni-vo-vetname/', permanent=True)),
+    path('%D0%BA%D0%B0%D0%BA-%D0%B0%D0%B4%D0%B0%D0%BF%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F-%D0%BA-%D0%B6%D0%B8%D0%B7%D0%BD%D0%B8-%D0%B2%D0%BE-%D0%92%D1%8C%D0%B5%D1%82%D0%BD%D0%B0%D0%BC%D0%B5', RedirectView.as_view(url='/kak-adaptirovatsya-k-zhizni-vo-vetname/', permanent=True)),
+    path('%D0%9A%D0%B0%D0%BA-%D0%B4%D0%BE%D0%B1%D1%80%D0%B0%D1%82%D1%8C%D1%81%D1%8F-%D0%B8%D0%B7-%D0%B0%D1%8D%D1%80%D0%BE%D0%BF%D0%BE%D1%80%D1%82%D0%B0-%D0%9A%D0%B0%D0%BC%D1%80%D0%B0%D0%BD%D1%8C-%D0%B4%D0%BE-%D0%9D%D1%8F%D1%87%D0%B0%D0%BD%D0%B3%D0%B0/', RedirectView.as_view(url='/kak-dobratsya-iz-aeroporta-kamran-do-nyachanga/', permanent=True)),
+    path('%D0%9A%D0%B0%D0%BA-%D0%B4%D0%BE%D0%B1%D1%80%D0%B0%D1%82%D1%8C%D1%81%D1%8F-%D0%B8%D0%B7-%D0%B0%D1%8D%D1%80%D0%BE%D0%BF%D0%BE%D1%80%D1%82%D0%B0-%D0%9A%D0%B0%D0%BC%D1%80%D0%B0%D0%BD%D1%8C-%D0%B4%D0%BE-%D0%9D%D1%8F%D1%87%D0%B0%D0%BD%D0%B3%D0%B0', RedirectView.as_view(url='/kak-dobratsya-iz-aeroporta-kamran-do-nyachanga/', permanent=True)),
+    path('%D0%BE%D0%B1%D0%BC%D0%B5%D0%BD-%D0%B2%D0%B0%D0%BB%D1%8E%D1%82%D1%8B-2025/', RedirectView.as_view(url='/obmen-valyuty-2025/', permanent=True)),
+    path('%D0%BE%D0%B1%D0%BC%D0%B5%D0%BD-%D0%B2%D0%B0%D0%BB%D1%8E%D1%82%D1%8B-2025', RedirectView.as_view(url='/obmen-valyuty-2025/', permanent=True)),
+    path('%D0%B2%D1%81%D0%B5-%D0%BE%D0%B1-%D0%B0%D1%80%D0%B5%D0%BD%D0%B4%D0%B5-%D1%82%D1%80%D0%B0%D0%BD%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%B0-%D0%B2%D0%BE-%D0%B2%D1%8C%D0%B5%D1%82%D0%BD%D0%B0%D0%BC%D0%B5/', RedirectView.as_view(url='/vse-ob-arende-transporta-vo-vetname/', permanent=True)),
+    path('%D0%B2%D1%81%D0%B5-%D0%BE%D0%B1-%D0%B0%D1%80%D0%B5%D0%BD%D0%B4%D0%B5-%D1%82%D1%80%D0%B0%D0%BD%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%B0-%D0%B2%D0%BE-%D0%B2%D1%8C%D0%B5%D1%82%D0%BD%D0%B0%D0%BC%D0%B5', RedirectView.as_view(url='/vse-ob-arende-transporta-vo-vetname/', permanent=True)),
+    path('%D0%BF%D0%BB%D1%8F%D0%B6%D0%B8-%D0%BD%D1%8F%D1%87%D0%B0%D0%BD%D0%B3%D0%B0-%D0%B1%D0%B5%D0%B7-%D0%B2%D0%BE%D0%BB%D0%BD/', RedirectView.as_view(url='/plyazhi-nyachanga-bez-voln-kak-dobratsya-interesnye-fakty/', permanent=True)),
+    path('%D0%BF%D0%BB%D1%8F%D0%B6%D0%B8-%D0%BD%D1%8F%D1%87%D0%B0%D0%BD%D0%B3%D0%B0-%D0%B1%D0%B5%D0%B7-%D0%B2%D0%BE%D0%BB%D0%BD', RedirectView.as_view(url='/plyazhi-nyachanga-bez-voln-kak-dobratsya-interesnye-fakty/', permanent=True)),
+    path('%D0%BD%D0%B0%D0%B9%D1%82%D0%B8-%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D1%8B-%D0%B2%D0%BE-%D0%92%D1%8C%D0%B5%D1%82%D0%BD%D0%B0%D0%BC%D0%B5/', RedirectView.as_view(url='/naiti-kvartiry-vietname-soveti-rekomendatsi/', permanent=True)),
+    path('%D0%BD%D0%B0%D0%B9%D1%82%D0%B8-%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D1%8B-%D0%B2%D0%BE-%D0%92%D1%8C%D0%B5%D1%82%D0%BD%D0%B0%D0%BC%D0%B5', RedirectView.as_view(url='/naiti-kvartiry-vietname-soveti-rekomendatsi/', permanent=True)),
+    path('%D0%BB%D1%83%D1%87%D1%88%D0%B8%D0%B5-%D0%B1%D1%83%D1%84%D0%B5%D1%82%D1%8B-%D0%BD%D1%8F%D1%87%D0%B0%D0%BD%D0%B3%D0%B0/', RedirectView.as_view(url='/luchshie-bufety-nyachanga-gde-poest/', permanent=True)),
+    path('%D0%BB%D1%83%D1%87%D1%88%D0%B8%D0%B5-%D0%B1%D1%83%D1%84%D0%B5%D1%82%D1%8B-%D0%BD%D1%8F%D1%87%D0%B0%D0%BD%D0%B3%D0%B0', RedirectView.as_view(url='/luchshie-bufety-nyachanga-gde-poest/', permanent=True)),
+    path('%D0%BC%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D0%B0-%D0%B2%D0%BE-%D0%92%D1%8C%D0%B5%D1%82%D0%BD%D0%B0%D0%BC%D0%B5/', RedirectView.as_view(url='/medicina-vietnam-nado-znat/', permanent=True)),
+    path('%D0%BC%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D0%B0-%D0%B2%D0%BE-%D0%92%D1%8C%D0%B5%D1%82%D0%BD%D0%B0%D0%BC%D0%B5', RedirectView.as_view(url='/medicina-vietnam-nado-znat/', permanent=True)),
+    path('%D0%BA%D0%B0%D0%BA-%D0%BD%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D1%82%D1%8C-%D0%B2%D0%BE-%D0%92%D1%8C%D0%B5%D1%82%D0%BD%D0%B0%D0%BC%D0%B5/', RedirectView.as_view(url='/kak-nachat-rabotat-vo-vietname/', permanent=True)),
+    path('%D0%BA%D0%B0%D0%BA-%D0%BD%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D1%82%D1%8C-%D0%B2%D0%BE-%D0%92%D1%8C%D0%B5%D1%82%D0%BD%D0%B0%D0%BC%D0%B5', RedirectView.as_view(url='/kak-nachat-rabotat-vo-vietname/', permanent=True)),
+    
+    # OpenCart формат index.php
+    path('index.php', RedirectView.as_view(url='/', permanent=True)),
     
     path(f'{settings.ADMIN_URL}/', admin.site.urls),
 
