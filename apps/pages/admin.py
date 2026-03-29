868001c1e9fbe15733d546b3129aa8ddbe83c768
colizej <colizej@gmail.com>
2026-03-29 13:44:53 +0200

    fix(pages): hide HTML content field in admin, show only Markdown

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -8,3 +8,13 @@ class PageAdmin(admin.ModelAdmin):
     list_filter = ('is_published', 'noindex')
     list_editable = ('is_published',)
     prepopulated_fields = {'slug': ('title',)}
+    
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'is_published', 'noindex', 'sort_order')
+        }),
+        ('Контент', {
+            'fields': ('content_md',),
+            'description': 'Используйте Markdown для форматирования контента.'
+        }),
+    )
