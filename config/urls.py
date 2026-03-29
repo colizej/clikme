112277df7da519a007d6c2161ddd70896822b27c
colizej <colizej@gmail.com>
2026-03-29 13:32:53 +0200

    fix(urls): remove infinite redirect for politika-konfidencialnosti

--- config/urls.py
+++ config/urls.py
@@ -22,8 +22,6 @@ urlpatterns = [
     path('about_us', RedirectView.as_view(url='/contacts/', permanent=True)),
     path('delivery/', RedirectView.as_view(url='/contacts/', permanent=True)),
     path('delivery', RedirectView.as_view(url='/contacts/', permanent=True)),
-    path('politika-konfidencialnosti/', RedirectView.as_view(url='/politika-konfidencialnosti/', permanent=True)),
-    path('politika-konfidencialnosti', RedirectView.as_view(url='/politika-konfidencialnosti/', permanent=True)),
     
     # Кириллические URL -> латиница
     path('%D0%BA%D0%B0%D0%BA-%D0%B0%D0%B4%D0%B0%D0%BF%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F-%D0%BA-%D0%B6%D0%B8%D0%B7%D0%BD%D0%B8-%D0%B2%D0%BE-%D0%92%D1%8C%D0%B5%D1%82%D0%BD%D0%B0%D0%BC%D0%B5/', RedirectView.as_view(url='/kak-adaptirovatsya-k-zhizni-vo-vetname/', permanent=True)),
